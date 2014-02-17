aae52688f7b8fc0b0579bb719e6a60d5e53600dd
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-02-17 21:58:18 +0400

    Первая домашка, первая задача
    дубль..хм..
    ?

---
+++ homework1_task1/homework1_task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1_task1
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please input number ");
+            int number = 0;
+            number = System.Int32.Parse(System.Console.ReadLine());
+            int result = Factorial(number);
+            System.Console.WriteLine("Factorial of this number = {0}", result);
+        }
+    }
+}
