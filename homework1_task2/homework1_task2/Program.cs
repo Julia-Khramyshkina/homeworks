baa4dbf933d7d7720e9d8a3765869c4425c2c2d5
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-02-18 20:44:39 +0400

    домашняя работа 1, задача 2 дубль 1

---
+++ homework1_task2/homework1_task2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace homework1_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            int fibonacchiPred = 1;
+	        int fibonacchiSled = 1;   
+            System.Console.WriteLine("Please input number Fibonacchi");
+	        int numberOfChisloFibonacchi = System.Int32.Parse(System.Console.ReadLine());
+	        if (numberOfChisloFibonacchi == 0) 
+            {
+		        System.Console.WriteLine("Chislo Fibonacchi {0}", fibonacchiPred);
+		        return;
+	        }
+            else if (numberOfChisloFibonacchi == 1) 
+            {
+		        System.Console.WriteLine("Chislo Fibonacchi {0}", fibonacchiSled);	
+		        return;
+	        }
+	        int chisloFibonacchi = 0;
+	        for (int i = 2; i <= numberOfChisloFibonacchi; ++i)
+            {
+		        chisloFibonacchi = fibonacchiSled + fibonacchiPred;
+		        fibonacchiPred = fibonacchiSled;
+		        fibonacchiSled = chisloFibonacchi;
+	        }
+	        System.Console.WriteLine("Chislo Fibonacchi {0}", chisloFibonacchi);
+        }
+    }
+}
