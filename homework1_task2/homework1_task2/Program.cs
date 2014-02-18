213b7cf0e840a819d7d1562e9efdc9154c661bca
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-02-18 20:53:41 +0400

    мм..форматирование.. поправки

--- homework1_task2/homework1_task2/Program.cs
+++ homework1_task2/homework1_task2/Program.cs
@@ -5,27 +5,27 @@
         static void Main(string[] args)
         {  
             int fibonacchiPred = 1;
-	        int fibonacchiSled = 1;   
+	    int fibonacchiSled = 1;   
             System.Console.WriteLine("Please input number Fibonacchi");
-	        int numberOfChisloFibonacchi = System.Int32.Parse(System.Console.ReadLine());
-	        if (numberOfChisloFibonacchi == 0) 
+	    int numberOfChisloFibonacchi = System.Int32.Parse(System.Console.ReadLine());
+	    if (numberOfChisloFibonacchi == 0) 
             {
-		        System.Console.WriteLine("Chislo Fibonacchi {0}", fibonacchiPred);
-		        return;
-	        }
+		System.Console.WriteLine("Chislo Fibonacchi {0}", fibonacchiPred);
+		return;
+	    }
             else if (numberOfChisloFibonacchi == 1) 
             {
-		        System.Console.WriteLine("Chislo Fibonacchi {0}", fibonacchiSled);	
-		        return;
-	        }
-	        int chisloFibonacchi = 0;
-	        for (int i = 2; i <= numberOfChisloFibonacchi; ++i)
+		System.Console.WriteLine("Chislo Fibonacchi {0}", fibonacchiSled);	
+		return;
+	    }
+	    int chisloFibonacchi = 0;
+	    for (int i = 2; i <= numberOfChisloFibonacchi; ++i)
             {
-		        chisloFibonacchi = fibonacchiSled + fibonacchiPred;
-		        fibonacchiPred = fibonacchiSled;
-		        fibonacchiSled = chisloFibonacchi;
-	        }
-	        System.Console.WriteLine("Chislo Fibonacchi {0}", chisloFibonacchi);
+		chisloFibonacchi = fibonacchiSled + fibonacchiPred;
+		fibonacchiPred = fibonacchiSled;
+		fibonacchiSled = chisloFibonacchi;
+	    }
+	    System.Console.WriteLine("Chislo Fibonacchi {0}", chisloFibonacchi);
         }
     }
 }
