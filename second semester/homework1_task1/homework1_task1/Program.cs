5b3042b63ce892310914350d2108d2a7486bc67f
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-02-19 22:19:42 +0400

    Second Semester

---
+++ second semester/homework1_task1/homework1_task1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace homework1_task1
+{
+    class Program
+    {
+        // метод, осуществляющий вычисление факториала
+        static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please input number ");
+            int number  = System.Int32.Parse(System.Console.ReadLine());
+            int result = Factorial(number);
+            System.Console.WriteLine("Factorial of this number = {0}", result);
+        }
+    }
+}
---
+++ second semester/homework1_task6/homework1_task6/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+
+namespace homework1_task6
+{
+    class Program
+    {
+        // распечатка части массива при движении вправо
+        static int PrintToRight(int[,] array, int positionI, int positionJ, int step)
+        { 
+            int counter = 0;
+            while (counter != step)
+            {
+                ++positionJ;
+                ++counter;
+                System.Console.Write("{0} ", array[positionI, positionJ]);            
+            }
+            return positionJ;
+        }
+        // распечатка части массива при движении вниз
+        static int PrintToDown(int[,] array, int positionI, int positionJ, int step)
+        {
+            int counter = 0;
+            while (counter != step)
+            {
+                ++positionI;
+                ++counter;
+                System.Console.Write("{0} ", array[positionI, positionJ]);        
+            }
+            return positionI;
+        }
+        // распечатка части массива при движении влево
+        static int PrintToLeft(int[,] array, int positionI, int positionJ, int step)
+        {
+            int counter = 0;
+            while (counter != step)
+            {
+                --positionI;
+                ++counter;
+                System.Console.Write("{0} ", array[positionI, positionJ]);      
+            }
+            return positionI;
+        }
+        // распечатка части массива при движении вверх
+        static int PrintToUp(int[,] array, int positionI, int positionJ, int step)
+        {
+            int counter = 0;
+            while (counter != step)
+            {
+                --positionJ;
+                ++counter;
+                System.Console.Write("{0} ", array[positionI, positionJ]);  
+            }
+            return positionJ;
+        }
+
+        static void Main(string[] args)
+        {
+            const string fileName = "input.txt";
+            BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open));
+            int size = reader.ReadInt32();
+            int[,] array = new int[size, size];
+            for (int i = 0; i < size; ++i)
+                for (int j = 0; j < size; ++j)
+                {
+                    array[i, j] = reader.ReadInt32();
+                }
+            int start = size / 2 + 1;
+            int step = 1;
+            System.Console.Write("{0} ", array[start, start]);
+            int positionI = start;
+            int positionJ = start;
+            while (step != size)
+            {
+                positionJ = PrintToRight(array, positionI, positionJ, step);
+                positionI = PrintToDown(array, positionI, positionJ, step);
+                ++step;
+                positionJ = PrintToLeft(array, positionI, positionJ, step);
+                positionI = PrintToUp(array, positionI, positionJ, step);
+                if (step + 1 == size)
+                {
+                    PrintToRight(array, positionI, positionJ, step);           
+                }
+                ++step;
+            }
+        }
+    }
+}
