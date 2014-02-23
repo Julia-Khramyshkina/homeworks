8543e21739ae6e38e5d14dde8781bd3201dde7f8
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-02-23 16:38:00 +0400

    Правка треша с отступами и вынесенние вычисления чисел Фибоначчи в метод

--- second semester/homework1_task2/homework1_task2/Program.cs
+++ second semester/homework1_task2/homework1_task2/Program.cs
@@ -1,31 +1,38 @@
 ﻿namespace homework1_task2
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {  
-            int fibonacchiPred = 1;
-	    int fibonacchiSled = 1;   
-            System.Console.WriteLine("Please input number Fibonacchi");
-	    int numberOfChisloFibonacchi = System.Int32.Parse(System.Console.ReadLine());
-	    if (numberOfChisloFibonacchi == 0) 
-            {
-		System.Console.WriteLine("Chislo Fibonacchi {0}", fibonacchiPred);
-		return;
-	    }
-            else if (numberOfChisloFibonacchi == 1) 
-            {
-		System.Console.WriteLine("Chislo Fibonacchi {0}", fibonacchiSled);	
-		return;
-	    }
-	    int chisloFibonacchi = 0;
-	    for (int i = 2; i <= numberOfChisloFibonacchi; ++i)
-            {
-		chisloFibonacchi = fibonacchiSled + fibonacchiPred;
-		fibonacchiPred = fibonacchiSled;
-		fibonacchiSled = chisloFibonacchi;
-	    }
-	    System.Console.WriteLine("Chislo Fibonacchi {0}", chisloFibonacchi);
-        }
-    }
+	class Program
+	{
+		// calculation of Fibonacci numbers
+		static int Fibonacci(int indexNumberOfFibonacchi, int fibonacciPrevious, int fibonacciNext)
+		{
+			int numberFibonacci = 0;
+			for (int i = 2; i <= indexNumberOfFibonacchi; ++i)
+			{
+				numberFibonacci = fibonacciNext + fibonacciPrevious;
+				fibonacciPrevious = fibonacciNext;
+				fibonacciNext = numberFibonacci;
+			}
+			return numberFibonacci;
+ 		}
+
+		static void Main(string[] args)
+		{
+			int fibonacciPrevious = 1;
+			int fibonacciNext = 1;
+			System.Console.WriteLine("Please input number Fibonacchi");
+			int indexNumberOfFibonacci = System.Int32.Parse(System.Console.ReadLine());
+			if (indexNumberOfFibonacci == 0)
+			{
+				System.Console.WriteLine("Chislo Fibonacchi {0}", fibonacciPrevious);
+				return;
+			}
+ 			else if (indexNumberOfFibonacci == 1)
+			{
+				System.Console.WriteLine("Chislo Fibonacchi {0}", fibonacciNext);
+				return;
+			}
+			int numberFibonacchi = Fibonacci(indexNumberOfFibonacci, fibonacciPrevious, fibonacciNext);
+			System.Console.WriteLine("Chislo Fibonacchi {0}", numberFibonacchi);
+		}
+	}
 }
