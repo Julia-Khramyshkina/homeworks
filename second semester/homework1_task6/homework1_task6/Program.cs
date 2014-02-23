cc7df6c107041fb773973a1b245a39784b8644fb
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-02-23 21:07:00 +0400

    кажется, рабочая и адекватная версия

--- second semester/homework1_task6/homework1_task6/Program.cs
+++ second semester/homework1_task6/homework1_task6/Program.cs
@@ -7,71 +7,75 @@ namespace Homework1_task6
 {
     class Program
     {
-        static void WalkingAlongTheHelix(int[,] array, int positionI, int positionJ, int step, int size)
+        // распечатка части массива при движении вправо
+        static int PrintToRight(int[,] array, int positionI, int positionJ, int step, int size)
+        {
+            int counter = 0;
+            while (counter != step)
+            {
+                ++positionJ;
+                ++counter;
+                if (positionJ == size)
+                    break;
+                System.Console.Write("{0} ", array[positionI, positionJ]);
+            }
+            return positionJ;
+        }
+
+        // распечатка части массива при движении вниз
+        static int PrintToDown(int[,] array, int positionI, int positionJ, int step, int size)
+        {
+            int counter = 0;
+            while (counter != step)
+            {
+                ++positionI;
+                ++counter;
+                if (positionI == size)
+                    break;
+                System.Console.Write("{0} ", array[positionI, positionJ]);
+            }
+            return positionI;
+        }
+        
+        // распечатка части массива при движении влево
+        static int PrintToLeft(int[,] array, int positionI, int positionJ, int step, int size)
+        {
+            int counter = 0;
+            while (counter != step)
+            {
+                --positionJ;
+                ++counter;
+                if (positionJ < 0)
+                    break;
+                System.Console.Write("{0} ", array[positionI, positionJ]);
+            }
+            return positionJ;
+        }
+        
+        // распечатка части массива при движении вверх
+        static int PrintToUp(int[,] array, int positionI, int positionJ, int step, int size)
+        {
+            int counter = 0;
+            while (counter != step)
+            {
+                --positionI;
+                ++counter;
+                if (positionI < 0)
+                    break;
+                System.Console.Write("{0} ", array[positionI, positionJ]);
+            }
+            return positionI;
+        }
+
+        // обход по спирали
+        static void WalkingAlongTheHelix(int[,] array, int size)
         {
             int start = size / 2;
             int step = 1;
             System.Console.Write("{0} ", array[start, start]);
             int positionI = start;
             int positionJ = start;
-              // распечатка части массива при движении вправо
-            static int PrintToRight(int[,] array, int positionI, int positionJ, int step, int size)
-            {
-                int counter = 0;
-                while (counter != step)
-                {
-                    ++positionJ;
-                    ++counter;
-                    if (positionJ == size)
-                        break;
-                    System.Console.Write("{0} ", array[positionI, positionJ]);
-                }
-                return positionJ;
-            }
-            // распечатка части массива при движении вниз
-            static int PrintToDown(int[,] array, int positionI, int positionJ, int step, int size)
-            {
-                int counter = 0;
-                while (counter != step)
-                {
-                    ++positionI;
-                    ++counter;
-                    if (positionI == size)
-                        break;
-                    System.Console.Write("{0} ", array[positionI, positionJ]);
-                }
-                return positionI;
-            }
-        // распечатка части массива при движении влево
-            static int PrintToLeft(int[,] array, int positionI, int positionJ, int step, int size)
-            {
-                int counter = 0;
-                while (counter != step)
-                {
-                    --positionJ;
-                    ++counter;
-                    if (positionJ < 0)
-                        break;
-                    System.Console.Write("{0} ", array[positionI, positionJ]);
-                }
-                return positionJ;
-            }
-        // распечатка части массива при движении вверх
-            static int PrintToUp(int[,] array, int positionI, int positionJ, int step, int size)
-            {
-                int counter = 0;
-                while (counter != step)
-                {
-                    --positionI;
-                    ++counter;
-                    if (positionI < 0)
-                        break;
-                    System.Console.Write("{0} ", array[positionI, positionJ]);
-                }
-                return positionI;
-            }
-
-         while (step != size)
+            while (step != size)
             {
                 positionJ = PrintToRight(array, positionI, positionJ, step, size);
                 positionI = PrintToDown(array, positionI, positionJ, step, size);
@@ -85,14 +89,14 @@ namespace Homework1_task6
                 ++step;
             }
         }
+
         static void Main(string[] args)
         {
             StreamReader input = new StreamReader("input.txt");
             string line;
             string[] buf;
             int size = int.Parse(input.ReadLine());
-            int [,] array = new int [size, size];
-           
+            int [,] array = new int [size, size];   
             int i = 0;
             while ((line = input.ReadLine()) != null)
             {
@@ -105,8 +109,7 @@ namespace Homework1_task6
                 ++i;
                 Console.WriteLine();
             }
-
-           
+            WalkingAlongTheHelix(array, size);          
         }
     }
 }
