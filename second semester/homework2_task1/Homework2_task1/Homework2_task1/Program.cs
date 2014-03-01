0aa093569921644aac25b3151b67b393b32506eb
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-03-01 13:32:35 +0400

    2 домашка, 1 задача :)

---
+++ second semester/homework2_task1/Homework2_task1/Homework2_task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework2_task1
+{
+    using NameSpaceForStack;
+    class Program
+    {
+        static void Main()
+        {
+            Stack myStack = new Stack();
+            for (int i = 0; i < 10; ++i)
+            {
+                myStack.Push(i);
+            }
+
+            while (!myStack.IsEmpty())
+            {
+                Console.WriteLine(myStack.Pop());
+            }
+            return;
+        }
+    }
+}
---
+++ second semester/homework2_task1/Homework2_task1/Homework2_task1/Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSpaceForStack
+{
+    public class Stack
+    {
+        private class StackElement
+        {
+            private int aValue;
+
+            public int Value
+            {
+                get
+                {
+                    return aValue;
+                }
+
+                set
+                {
+                    this.aValue = value;
+                }
+            }
+            public StackElement Next { get; set; }
+        }
+
+        private StackElement head = null;
+
+        public void Push(int value)
+        {
+            var newElement = new StackElement()
+            {
+                Next = head,
+                Value = value
+            };
+
+            head = newElement;
+        }
+
+        public int Pop()
+        {
+            if (head == null)
+            {
+                return -1;
+            }
+
+            var temp = head.Value;
+            head = head.Next;
+            return temp;
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+    }
+}
