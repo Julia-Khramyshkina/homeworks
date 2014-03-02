be6d9765af917d52161d4567d58056ec0684011e
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-03-02 14:00:10 +0400

    2 домашка, 3 задача, руки из "правильного" места, жизньболь. а вообще.. напишу на почту..)

---
+++ second semester/homework2_task3/Homework2_task3/Homework2_task3/Hash.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSpaceForHash
+{
+    using NameSpaceForList;
+    public class HashTable
+    {
+        public int sizeOfHash = 500;
+        public class HashElement
+        {
+            public HashElement()
+            {
+                List hashElement = new List();
+
+               // public
+            }
+     
+       //    public HashElement Next { get; set; }
+            
+        }
+        public HashTable()
+        {
+            HashElement [] hashTable = new HashElement[sizeOfHash];
+            for (int i = 0; i < sizeOfHash; ++i)
+            {
+                hashTable[i] = new HashElement();
+            }
+        }
+
+
+        public int hashFunction(int newElement)
+        {
+            int valueOfHashFunction = 0;
+            for (int i = 0; i < newElement; ++i)
+            {
+                valueOfHashFunction = valueOfHashFunction + newElement * i;
+            }
+            valueOfHashFunction = (valueOfHashFunction * ((newElement * newElement) + (newElement / 3) - 1)) % sizeOfHash;
+            return valueOfHashFunction;
+        }
+        
+        public void insertToHashTable(int newElement)
+        {
+           
+        }
+
+
+    }
+}
---
+++ second semester/homework2_task3/Homework2_task3/Homework2_task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_task3
+{
+    using NameSpaceForHash;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable hashTable = new HashTable();
+            hashTable.insertToHashTable(12345);
+            hashTable.insertToHashTable(1);
+            hashTable.insertToHashTable(2);
+            hashTable.insertToHashTable(3);
+            return;
+
+
+        }
+    }
+}
