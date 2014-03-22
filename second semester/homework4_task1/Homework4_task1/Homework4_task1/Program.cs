43963b58b5b5c76300e35285999aeeabedd59f8d
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-03-22 18:08:52 +0400

    дерево..начало)

---
+++ second semester/homework4_task1/Homework4_task1/Homework4_task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            tree.InsertElementOfInt(5);
+
+            tree.InsertElementOfInt(2);
+            tree.InsertElementOfInt(1);
+            tree.InsertElementOfInt(4);
+            tree.InsertElementOfInt(6);
+
+        }
+    }
+}
---
+++ second semester/homework4_task1/Homework4_task1/Homework4_task1/Tree.cs
@@ -0,0 +1,86 @@
+﻿namespace Homework4_task1
+{
+    public class Tree
+    {
+       
+        private class ElementOfTree
+        {
+            public int ValueOfChar { get; set; }
+            public int ValueOfInt { get; set; }
+            public ElementOfTree(char valueOfChar, int valueOfInt)
+            {
+                ValueOfChar = valueOfChar;
+                ValueOfInt = valueOfInt;
+                Left = null;
+                Right = null;
+            }
+        
+            public ElementOfTree Left { get; set; }
+            public ElementOfTree Right { get; set; }
+        }
+        private ElementOfTree head = null;
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        public void InsertElementOfInt(int newElement)
+        {
+            var newElementOfTree = new ElementOfTree('0', newElement);
+            if (IsEmpty())
+            {
+                head = newElementOfTree;
+                return;
+            }
+            var tempElement = this.head;
+            bool less = true;
+            while (true)
+            {
+                if (newElement < tempElement.ValueOfInt)
+                {
+                    less = true;
+                    if (tempElement.Left != null)
+                    {
+                        tempElement = tempElement.Left;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    less = false;
+                    if (tempElement.Right != null)
+                    {
+                        tempElement = tempElement.Right;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    tempElement = tempElement.Right;
+                }
+            }
+
+            if (less)
+            {
+                tempElement.Left = new ElementOfTree('0', newElement);
+            }
+            else
+            {
+                tempElement.Right = new ElementOfTree('0', newElement);
+
+            }
+   
+           
+        }
+
+
+
+
+
+
+    }
+}
