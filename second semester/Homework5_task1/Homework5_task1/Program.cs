d4f6d7a7361e4f215c0584f474af68b9fc813c6f
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-03-24 23:46:49 +0400

    а тем временем.. мм.. разного рода идеи, попытки.. )

---
+++ second semester/Homework5_task1/Homework5_task1/Program.cs
@@ -0,0 +1,47 @@
+﻿//using System.MulticastDelegate;
+//using System.Delegate;
+//using System.Linq.Expressions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Homework5_task1
+{
+    class Program
+    {
+        Func<int, int> x2 = x => x * 2;
+
+        public static void Map(List list, Func<int,int> x2)
+        {
+            int end = list.SizeOfList();   
+            var temp = list.First();
+            for (int i = 0; i < end; ++i)
+            {
+                temp.Value = x2(temp.Value);
+                temp = temp.Next;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            List list = new List();
+
+            for (int i = 0; i < 4; ++i)
+            {
+                list.InsertToEnd(i);
+            }
+
+            list.PrintList();
+
+          //  Map(list, Func<int,int> x2);
+            list.PrintList();
+        }
+    }
+}
+
