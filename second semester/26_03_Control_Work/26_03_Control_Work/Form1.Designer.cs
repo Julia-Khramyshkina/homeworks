bccf37999ca96611caa141fbe05fce405aa27242
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-03-26 12:36:41 +0400

    мм, ну, вот как-то так..:)

---
+++ second semester/26_03_Control_Work/26_03_Control_Work/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace _26_03_Control_Work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.AutoScrollMinSize = new System.Drawing.Size(200, 200);
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button9, 2, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(346, 312);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.TableLayoutPanel1Paint);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 97);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(118, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 97);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(233, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 97);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3Click);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Location = new System.Drawing.Point(3, 106);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 97);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4Click);
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Location = new System.Drawing.Point(118, 106);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 97);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5Click);
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Location = new System.Drawing.Point(233, 106);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 97);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6Click);
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Location = new System.Drawing.Point(3, 209);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(109, 100);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7Click);
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button8.Location = new System.Drawing.Point(118, 209);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(109, 100);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8Click);
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button9.Location = new System.Drawing.Point(233, 209);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(110, 100);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 312);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(250, 250);
+            this.Name = "Form1";
+            this.Text = "Tic-tac-toe";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ second semester/26_03_Control_Work/26_03_Control_Work/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _26_03_Control_Work
+{
+    public partial class Form1 : Form
+    {
+        private bool click = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Сreating a place to play.
+        /// </summary>
+        /// <param name="sender">Transmitted object.</param>
+        /// <param name="e">Transmitted args.</param>
+        private void TableLayoutPanel1Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Processing pressing a key № 1.
+        /// </summary>
+        /// <param name="sender">Transmitted object.</param>
+        /// <param name="e">Transmitted args.</param>
+        private void Button1Click(object sender, EventArgs e)
+        {
+            if (this.button1.Text != "")
+                return;
+
+            if (click)
+            {
+                this.button1.Text = "X";
+                click = !click;
+            }
+            else
+            {
+                this.button1.Text = "O";
+                click = !click;
+            }
+        }
+
+        /// <summary>
+        /// Processing pressing a key № 2.
+        /// </summary>
+        /// <param name="sender">Transmitted object.</param>
+        /// <param name="e">Transmitted args.</param>
+        private void Button2Click(object sender, EventArgs e)
+        {
+            if (this.button2.Text != "")
+                return;
+
+            if (click)
+            {
+                this.button2.Text = "X";
+                click = !click;
+            }
+            else
+            {
+                this.button2.Text = "O";
+                click = !click;
+            }
+        }
+
+        /// <summary>
+        /// Processing pressing a key № 3.
+        /// </summary>
+        /// <param name="sender">Transmitted object.</param>
+        /// <param name="e">Transmitted args.</param>
+        private void Button3Click(object sender, EventArgs e)
+        {
+            if (this.button3.Text != "")
+                return;
+
+            if (click)
+            {
+                this.button3.Text = "X";
+                click = !click;
+            }
+            else
+            {
+                this.button3.Text = "O";
+                click = !click;
+            }
+        }
+
+        /// <summary>
+        /// Processing pressing a key № 4.
+        /// </summary>
+        /// <param name="sender">Transmitted object.</param>
+        /// <param name="e">Transmitted args.</param>
+        private void Button4Click(object sender, EventArgs e)
+        {
+            if (this.button4.Text != "")
+                return;
+
+            if (click)
+            {
+                this.button4.Text = "X";
+                click = !click;
+            }
+            else
+            {
+                this.button4.Text = "O";
+                click = !click;
+            }
+        }
+
+        /// <summary>
+        /// Processing pressing a key № 5.
+        /// </summary>
+        /// <param name="sender">Transmitted object.</param>
+        /// <param name="e">Transmitted args.</param>
+        private void Button5Click(object sender, EventArgs e)
+        {
+            if (this.button5.Text != "")
+                return;
+
+            if (click)
+            {
+                this.button5.Text = "X";
+                click = !click;
+            }
+            else
+            {
+                this.button5.Text = "O";
+                click = !click;
+            }
+        }
+
+        /// <summary>
+        /// Processing pressing a key № 6.
+        /// </summary>
+        /// <param name="sender">Transmitted object.</param>
+        /// <param name="e">Transmitted args.</param>
+        private void Button6Click(object sender, EventArgs e)
+        {
+            if (this.button6.Text != "")
+                return;
+
+            if (click)
+            {
+                this.button6.Text = "X";
+                click = !click;
+            }
+            else
+            {
+                this.button6.Text = "O";
+                click = !click;
+            }
+        }
+
+        /// <summary>
+        /// Processing pressing a key № 7.
+        /// </summary>
+        /// <param name="sender">Transmitted object.</param>
+        /// <param name="e">Transmitted args.</param>
+        private void Button7Click(object sender, EventArgs e)
+        {
+            if (this.button7.Text != "")
+                return;
+
+            if (click)
+            {
+                this.button7.Text = "X";
+                click = !click;
+            }
+            else
+            {
+                this.button7.Text = "O";
+                click = !click;
+            }
+        }
+
+        /// <summary>
+        /// Processing pressing a key № 8.
+        /// </summary>
+        /// <param name="sender">Transmitted object.</param>
+        /// <param name="e">Transmitted args.</param>
+        private void Button8Click(object sender, EventArgs e)
+        {
+            if (this.button8.Text != "")
+                return;
+
+            if (click)
+            {
+                this.button8.Text = "X";
+                click = !click;
+            }
+            else
+            {
+                this.button8.Text = "O";
+                click = !click;
+            }
+        }
+
+        /// <summary>
+        /// Processing pressing a key № 9.
+        /// </summary>
+        /// <param name="sender">Transmitted object.</param>
+        /// <param name="e">Transmitted args.</param>
+        private void Button9Click(object sender, EventArgs e)
+        {
+            if (this.button9.Text != "")
+                return;
+
+            if (click)
+            {
+                this.button9.Text = "X";
+                click = !click;
+            }
+            else
+            {
+                this.button9.Text = "O";
+                click = !click;
+            }
+        }
+    }
+}
