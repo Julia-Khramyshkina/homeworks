b21998ac3c9f21ed72fb80731fa295aae1b36014
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-03-31 21:01:24 +0400

    какая-то часть)

---
+++ second semester/Homework5_task4/Homework5_task4/Action.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5_task4
+{
+    public class Action
+    {
+        private int origRow;
+        private int origCol;
+        public Action(int value1, int value2)
+        {
+            this.origRow = value1;
+            this.origCol = value2;
+        }
+        public void Movement(object sender, EventArgs args)
+        {
+
+            Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop);
+
+        }
+    }
+}
---
+++ second semester/Homework5_task4/Homework5_task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5_task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventLoop = new EventLoop();
+            Console.Clear();
+            Action action = new Action(Console.CursorLeft, Console.CursorTop);
+
+
+            eventLoop.LeftHandler += action.Movement;
+            eventLoop.RightHandler += action.Movement;
+            eventLoop.UpHandler += action.Movement;
+            eventLoop.DownHandler += action.Movement;
+ 
+
+            eventLoop.LeftHandler += (sender, eventArgs) => Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            eventLoop.RightHandler += (sender, eventArgs) => Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
+            eventLoop.UpHandler += (sender, eventArgs) => Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+            eventLoop.DownHandler += (sender, eventArgs) => Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop + 1);
+
+
+            eventLoop.Run();
+        }
+    }
+}
