3153904af7b88f4c2af2cb53dc8783ae9be511d2
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-04-06 18:38:07 +0400

    а вот так? :)

--- second semester/reCW/ReCW/ReCW/Form1.cs
+++ second semester/reCW/ReCW/ReCW/Form1.cs
@@ -12,8 +12,10 @@ namespace ReCW
 {
     public partial class Form1 : Form
     {
-        private Button[] buttons = new Button [9];
+        private Button[,] buttons = new Button [3, 3];
+
         private bool click = true;
+        private bool win = false;
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
@@ -43,14 +45,14 @@ namespace ReCW
             {
                 for (int j = 0; j < 3; ++j)
                 {
-                    buttons[number] = new System.Windows.Forms.Button();
-                    buttons[number].Location = new System.Drawing.Point(3 + 95 * i, 3 + 95 * j);
-                    buttons[number].Name = "button" + number.ToString();
-                    buttons[number].Size = new System.Drawing.Size(89, 81);
-                    buttons[number].TabIndex = number;
-                    buttons[number].UseVisualStyleBackColor = true;
-                    buttons[number].Click += new System.EventHandler(this.ButtonClick);
-                    this.tableLayoutPanel1.Controls.Add(buttons[number], i, j);
+                    buttons[i, j] = new System.Windows.Forms.Button();
+                    buttons[i, j].Location = new System.Drawing.Point(3 + 95 * i, 3 + 95 * j);
+                    buttons[i, j].Name = "button" + number.ToString();
+                    buttons[i, j].Size = new System.Drawing.Size(89, 81);
+                    buttons[i, j].TabIndex = number;
+                    buttons[i, j].UseVisualStyleBackColor = true;
+                    buttons[i, j].Click += new System.EventHandler(this.ButtonClick);
+                    this.tableLayoutPanel1.Controls.Add(buttons[i, j], i, j);
                     ++number;
                 }
             }
@@ -76,7 +78,8 @@ namespace ReCW
         private void ButtonClick(object sender, EventArgs e)
         {
             Button countButton = (Button)sender;
-            if (countButton.Text != "")
+
+            if (countButton.Text != "" || win)
                 return;
 
             if (click)
@@ -85,6 +88,7 @@ namespace ReCW
                 if (this.Win())
                 {
                     countButton.Text = "X, WIN";
+                    win = true;
                     return;
                 }
                 click = !click;
@@ -95,39 +99,36 @@ namespace ReCW
                 if (this.Win())
                 {
                     countButton.Text = "O, WIN";
+                    win = true;
+                    return;
                 }
                 click = !click;
             }
-
         }
 
         private bool Win()
         {
             
             for (int i = 0; i < 3; ++i)
-            {
-                for (int j = 0; j < 3; ++ j)
-                {
-                    if (buttons[i].Text == buttons[i + 1].Text &&  buttons[i + 1].Text
-                        == buttons[i + 2].Text && buttons[i + 2].Text != "")
-                        return true;
+            {             
+                if (buttons[0, i].Text == buttons[1, i].Text &&  buttons[1, i].Text
+                    == buttons[2, i].Text && buttons[2, i].Text != "")
+                    return true;
 
-                    if (buttons[j].Text == buttons[j + 3].Text && buttons[j + 3].Text
-                        == buttons[j + 6].Text && buttons[j + 6].Text != "")
-                        return true;
-                }
+                if (buttons[i, 0].Text == buttons[i, 1].Text && buttons[i, 1].Text
+                    == buttons[i, 2].Text && buttons[i, 2].Text != "")
+                    return true;
             }
 
-            if (buttons[0].Text == buttons[4].Text && buttons[4].Text == buttons[8].Text
-                && buttons[8].Text != "")
+            if (buttons[0, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 2].Text
+                && buttons[2, 2].Text != "")
                 return true;
 
-            if (buttons[2].Text == buttons[4].Text && buttons[4].Text == buttons[6].Text
-                && buttons[6].Text != "")
+            if (buttons[0, 2].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 0].Text
+                && buttons[2, 0].Text != "")
                 return true;
 
             return false;        
-
         }
 
 
