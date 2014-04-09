78c37f1eeeeffec2b3ea5a3fd6d9835f44be74f4
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-04-09 20:09:38 +0400

    и, видимо, от этого никуда не деться..

---
+++ second semester/LifeIsPain/LifeIsPain/LifeIsPain.Tests/PriorityQueueTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LifeIsPain.Tests
+{
+    [TestClass]
+    public class PriorityQueueTest
+    {
+        private PriorityQueue priorityQueue;
+        [TestInitialize]
+        public void Initialize()
+        {
+            priorityQueue = new PriorityQueue();
+        }
+
+        [TestMethod]
+        public void IsEmptyTest()
+        {
+            Assert.IsTrue(priorityQueue.IsEmpty());
+        }
+
+        [TestMethod]
+        public void EnqueueTest()
+        {
+            priorityQueue.Enqueue(1, 1);
+            Assert.IsFalse(priorityQueue.IsEmpty());
+        }
+
+        [TestMethod]
+        public void DequeueTest1()
+        {
+            priorityQueue.Enqueue(1, 1);
+            priorityQueue.Enqueue(2, 2);
+            Assert.AreEqual(2, priorityQueue.Dequeue());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void DequeueTest2()
+        {
+           priorityQueue.Dequeue();
+        }
+
+        [TestMethod]
+        public void DequeueTest3()
+        {
+            priorityQueue.Enqueue(1, 1);
+            priorityQueue.Enqueue(3, 3);
+            priorityQueue.Enqueue(2, 3);
+            Assert.AreEqual(3, priorityQueue.Dequeue());
+        }
+    }
+
+}
---
+++ second semester/LifeIsPain/LifeIsPain/LifeIsPain/PriorityQueue.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeIsPain
+{
+    public class PriorityQueue
+    {
+        private class QueueElement
+        {
+            public int Value { get; set; }
+            public int Priority { get; set; }
+            public QueueElement(int value, int priority)
+            {
+                Value = value;
+                Priority = priority;
+            }
+            public QueueElement Next { get; set; }
+        }
+        private QueueElement head = null;
+
+        /// <summary>
+        /// Checking queue. Empty?
+        /// </summary>
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        /// <summary>
+        /// Inser to queue.
+        /// </summary>
+        /// <param name="value">Value to be insert.</param>
+        /// <param name="priority">This priority.</param>
+        public void Enqueue(int value, int priority)
+        {
+            if (this.IsEmpty())
+            {
+                this.head = new QueueElement(value, priority);
+                return;
+            }
+
+            QueueElement tempElement = this.head;
+
+            if (tempElement.Next == null)
+            {
+                if (priority > this.head.Priority)
+                {
+                    var element = this.head;
+                    var thisElement = new QueueElement(value, priority);
+                    this.head = thisElement;
+                    this.head.Next = element;
+                    return;
+                }
+                else
+                {
+                    this.head.Next = new QueueElement(value, priority);
+                    return;
+                }
+            }
+            QueueElement tempElementPrevious = this.head;
+
+            while (tempElement.Priority >= priority)
+            {
+                if (tempElement.Next == null)
+                {
+                    break;
+                }        
+
+                if (tempElement.Next.Priority >= priority)
+                    tempElementPrevious = tempElementPrevious.Next;
+
+                tempElement = tempElement.Next;
+            }
+
+            QueueElement newElement = new QueueElement(value, priority);
+      
+            tempElementPrevious.Next = newElement;
+            newElement = tempElement.Next;
+        }
+
+        /// <summary>
+        /// Get value from queue.
+        /// </summary>
+        /// <returns></returns>
+        public int Dequeue()
+        {
+            if (this.IsEmpty())
+            {
+                throw new Exception("Queue is empty");
+            }
+
+            int value = this.head.Value;
+            this.head = this.head.Next;
+            return value;
+        }
+    }
+}
