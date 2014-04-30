87ff0d8d6d54b68b0534f7d40ee5e3ba5531f5f5
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-04-30 12:55:27 +0400

    можно поздороваться с миром.. или нет

---
+++ second semester/3_CW/3_CW/3_CW/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace _3_CW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Mouse Move to button
+        /// </summary>
+        /// <param name="sender">This object</param>
+        /// <param name="e">This params.</param>
+        private void MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.Location = new Point
+                (Math.Min(Math.Abs(this.Size.Width - 2 * button1.Location.X) * 3  %  this.Size.Width, 
+                Math.Abs(this.Size.Width - 2 * button1.Location.X) * 3  %  this.Size.Height),
+                Math.Min((Math.Abs(this.Size.Height - 2 * button1.Location.Y) * 3 %  this.Size.Height),                
+                Math.Abs(this.Size.Height - 2 * button1.Location.Y) * 3 %  this.Size.Width));
+        }
+
+        /// <summary>
+        /// Close the program.
+        /// </summary>
+        /// <param name="sender">This object.</param>
+        /// <param name="e">That params.</param>
+        private void ThisKeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Close the program with click in this butoon!
+        /// </summary>
+        /// <param name="sender">This object.</param>
+        /// <param name="e">This params.</param>
+        private void Button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ second semester/3_CW/3_CW/3_CW/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("3_CW")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("3_CW")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("6d817066-d84f-4be0-9947-1559e3036b56")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
