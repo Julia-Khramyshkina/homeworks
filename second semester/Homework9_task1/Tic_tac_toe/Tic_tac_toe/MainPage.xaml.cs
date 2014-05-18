68fd941bce9e40ff0e8e2b44dfcf095516c8fc80
Julia-Khramyshkina <ulkin_xram@mail.ru>
2014-05-18 15:43:07 +0400

    выиграть?..)

--- second semester/Homework9_task1/Tic_tac_toe/Tic_tac_toe/MainPage.xaml.cs
+++ second semester/Homework9_task1/Tic_tac_toe/Tic_tac_toe/MainPage.xaml.cs
@@ -15,53 +15,130 @@ namespace Tic_tac_toe
 {
     public partial class MainPage : UserControl
     {
-        
-        internal System.Windows.Controls.Button[,] buttons = new System.Windows.Controls.Button[3, 3];
-        //private bool click = true;
-        //private bool win = false;
+        internal System.Windows.Controls.Button[] buttons = new System.Windows.Controls.Button[9];
+        private int Change = 0;
+        private bool Win = false;
+
 
         public MainPage()
         {
             InitializeComponent();
+            buttons[0] = button1;
+            buttons[1] = button2;
+            buttons[2] = button3;
+            buttons[3] = button4;
+            buttons[4] = button5;
+            buttons[5] = button6;
+            buttons[6] = button7;
+            buttons[7] = button8;
+            buttons[8] = button9;
 
-            //this._1 = ((System.Windows.Controls.Button)(this.FindName("_1")));
-
-            int number = 0;
-            for (int i = 0; i < 3; ++i)
-            {
-                for (int j = 0; j < 3; ++j)
-                {
-                    //System.Windows.Controls.Button
-
-
-                    //buttons[i, j] = ((System.Windows.Controls.Button)(this.FindName("butt" + number.ToString())));
-                    //buttons[i, j].Content = "i";
-
-                          //  <Button x:Name="_1" Content="Button" HorizontalAlignment="Left" Margin="175,84,0,0" VerticalAlignment="Top" Width="75" Click="Button_Click_1"/>
-
-               //     this.LayoutRoot.
-                //    buttons[i, j]. = new System.Drawing.Point(3 + 95 * i, 3 + 95 * j);
-                    //buttons[i, j].Name = "button" + number.ToString();
-                 //   buttons[i, j].ActualHeight = new 81;
-                //    buttons[i, j].TabIndex = number;
-                //    buttons[i, j].UseVisualStyleBackColor = true;
-              //      buttons[i, j].Click += new System.EventHandler(this.Button_Click_1);
-
-                    //this.
-                    //this.tableLayoutPanel1.Controls.Add(buttons[i, j], i, j);
-                    ++number;
-                }
-            }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private bool IsWin()
         {
-            Button bt = (Button)sender;
+            for (int i = 0; i < 3; ++i)
+            {
+                if (buttons[i].Content == buttons[i + 3].Content && buttons[i + 3].Content == buttons[i + 6].Content && buttons[i + 6].Content.ToString() != "")
+                    return true;
+            }
+            int j = 0;
+            while (j < 7)
+            {
+                if (buttons[j].Content == buttons[j + 1].Content && buttons[j + 1].Content == buttons[j + 2].Content && buttons[j + 2].Content.ToString() != "")
+                    return true;
+                j = j + 3;
+            }
 
-            bt.Content = "1";
+            if (buttons[0].Content == buttons[4].Content && buttons[4].Content == buttons[8].Content && buttons[8].Content.ToString() != "")
+                return true;
+            if (buttons[2].Content == buttons[4].Content && buttons[4].Content == buttons[6].Content && buttons[6].Content.ToString() != "")
+                return true;
+
+            return false;
+
+        }
 
 
-            //bt.
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button countButton = (Button)sender;
+            if (this.Win)
+                return;
+
+            if (countButton.Name.ToString() == "Change0" && this.Change == 0)
+            {
+                this.Change = -1;
+                countButton.Content = countButton.Content + "!";
+                return;
+            }
+            if (this.Change == 0 && countButton.Name.ToString() == "ChangeX")
+            {
+                this.Change = 1;
+                countButton.Content = countButton.Content + "!";
+                return;
+            }
+            
+
+
+
+            if (countButton.Content.ToString() != "")
+                return;
+
+
+            if (this.Change == 1)
+            {
+                countButton.Content = "X";
+
+                if (this.IsWin())
+                {
+                    countButton.Content = "X! WIN";
+                    this.Win = true;
+                    return;
+                }                 
+            }
+            else
+            {
+                countButton.Content = "0";
+                if (this.IsWin())
+                {
+                    countButton.Content = "0! WIN";
+                    this.Win = true;
+                    return;
+                }
+            }
+
+
+
+            for (int i = 0; i < 9; ++i)
+            {
+                if (buttons[i].Content == "")
+                {
+                    if (this.Change == 1)
+                    {
+                        buttons[i].Content = "0";
+                        if (this.IsWin())
+                        {
+                            countButton.Content = "0! WIN";
+                            this.Win = true;
+                        }
+                        return;
+                    }
+                    else
+                    {
+                        buttons[i].Content = "X";
+                        if (this.IsWin())
+                        {
+                            countButton.Content = "X! WIN";
+                            this.Win = true;
+                        }     
+                        return;
+                    }
+                }
+            }
+      
+
+
         }
 
        
