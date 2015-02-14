e5003da663f7090d77bb6ffa8a949228f55642db
Julia-Khramyshkina <ulkin_xram@mail.ru>
2015-02-14 15:26:12 +0400

    task1_1 start version

---
+++ fourth semester/task1_1/task1_1/factorial.fs
@@ -0,0 +1,16 @@
+﻿
+//// Дополнительные сведения о F# см. на http://fsharp.net
+//// Дополнительную справку см. в проекте "Учебник по F#".
+//
+let main argv = 
+    printfn "%A" argv
+
+System.Console.WriteLine("Please enter a number for the factorial")
+let startValue = System.Console.ReadLine()
+let result : int = int startValue   
+
+let rec factorial n = 
+    if n = 0 
+    then 1 
+    else n * factorial(n - 1)
+System.Console.WriteLine(factorial result)
