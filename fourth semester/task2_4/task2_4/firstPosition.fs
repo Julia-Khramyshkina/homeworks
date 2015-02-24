0a9084644894cf7936e47fc297ef1e1361938608
Julia-Khramyshkina <ulkin_xram@mail.ru>
2015-02-24 15:30:41 +0400

    corrections[]

--- fourth semester/task2_4/task2_4/firstPosition.fs
+++ fourth semester/task2_4/task2_4/firstPosition.fs
@@ -1,15 +1,13 @@
-﻿let rec firstPositionOfNumberInList number list size  =
-    match list with
-    | [] -> -1
-    | h :: t ->
-      if h = number then
-         let temp = size - List.length t - 1
-         temp
-      else
-         firstPositionOfNumberInList number t size 
-
-let firstPosition number list =
-    let size = List.length list
-    firstPositionOfNumberInList number list size
+﻿let firstPosition number list =
+   let rec firstPositionOfNumberInList number list size  =
+       match list with
+       | [] -> -1
+       | h :: t ->
+         if h = number then
+            size - List.length t - 1
+         else
+            firstPositionOfNumberInList number t size 
+   let size = List.length list
+   firstPositionOfNumberInList number list size
 
 firstPosition 3 [1; 2; 3; 4; 5]
