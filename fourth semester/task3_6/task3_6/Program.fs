422d439ce1c2736592d4dd8b26b2a7d199e632f9
Julia-Khramyshkina <ulkin_xram@mail.ru>
2015-03-02 02:51:42 +0400

    small corrections[1]

--- fourth semester/task3_6/task3_6/Program.fs
+++ fourth semester/task3_6/task3_6/Program.fs
@@ -1,7 +1,7 @@
 ﻿let isPrime number = 
    let rec isPrimeRec number currentNumber =
        let currentNumber = currentNumber + 1
-       if (currentNumber < number && number % currentNumber <> 0) then 
+       if (currentNumber < (number / 2) && number % currentNumber <> 0) then 
          isPrimeRec number currentNumber
        elif (number % currentNumber = 0 && number <> currentNumber) then
          false
@@ -10,8 +10,6 @@
        else
          true
    isPrimeRec number 1
-
-isPrime 123
 
 let primes =
     Seq.initInfinite (fun i -> i + 2) 
