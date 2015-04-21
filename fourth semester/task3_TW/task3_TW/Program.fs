ad18dec37ccfd575a632cd13b6ed35c377653340
Julia-Khramyshkina <ulkin_xram@mail.ru>
2015-04-21 22:42:31 +0400

    change

--- fourth semester/task3_TW/task3_TW/Program.fs
+++ fourth semester/task3_TW/task3_TW/Program.fs
@@ -2,37 +2,22 @@
 open System.Collections.Generic
 
 type HashTable<'a when 'a: equality>(hashFunction : ('a -> int)) =
-   let retMutL i = 
-      let mutable list = new List<_>()
-      list    
-   let mutable hashtable = [for i in 0..499 -> retMutL i]
-   let ourHashFunction = hashFunction
-
+   let mutable hashtable = [for i in 0..499 -> new List<_>()]
    member v.Insert value = 
-      let ins value  =
-         let cell = ourHashFunction value 
-         hashtable.[cell].Add(value)
-      ins value
+      let cell = hashFunction value 
+      hashtable.[cell].Add(value)
 
    member v.Remove value =
-      let remove value = 
-         let cell = ourHashFunction value
-         (hashtable.Item cell).Remove(value)
-      remove value
+      let cell = hashFunction value
+      (hashtable.Item cell).Remove(value)
 
    member v.Exist value =
-      let exist value =
-         let cell = ourHashFunction value
-         let list = hashtable.Item cell
-         let length = List.length [list]
-         let mutable result = false
-         for i in 0..length - 1 do
-            if (list.[i]) = value then
-              result <- true
-         result
-      exist value
+      let cell = hashFunction value
+      let list = hashtable.Item cell
+      let res = list.Contains(value)
+      res
 
-let func = fun x -> (x * 5 + 2) % 499
+let func = fun x -> (x * 5 + 8) % 500
 
 let hashtable = new HashTable<int>(func)  
 hashtable.Insert 1
