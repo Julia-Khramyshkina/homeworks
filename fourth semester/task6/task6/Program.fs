a18aace5032c097ada50389da59f69efe9ce1ce4
Julia-Khramyshkina <ulkin_xram@mail.ru>
2015-04-25 22:45:14 +0400

    task 6

---
+++ fourth semester/task6/task6/Program.fs
@@ -0,0 +1,14 @@
+﻿type RoundingBuilder (accuracy : int) =
+    member this.Bind ((x : float) , rest) =
+         rest x   
+    member this.Return (x : float) = 
+        System.Math.Round(x, accuracy)
+
+let test = 
+   RoundingBuilder 3 {
+      let! a = 2.0 / 12.0
+      let! b = 3.5
+      return a / b
+   }
+
+test
