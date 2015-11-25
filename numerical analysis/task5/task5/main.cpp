06986fa0f6250d1e39cfecb590f9c983a390defb
Julia <ulkin_xram@mail.ru>
2015-11-25 12:27:10 +0300

    corrections

--- numerical analysis/task5/task5/main.cpp
+++ numerical analysis/task5/task5/main.cpp
@@ -11,18 +11,22 @@ using namespace std;
 double function(double x)
 {
 	return x * x + 2 * x + 1;
+	//return 2 * x + 1;
 }
 
 double I(double a, double b)
 {
 	return pow(b, 3.0) / 3.0 - pow(a, 3.0) / 3.0 +
 			(b * b - a * a) + (b - a);
+
+//	return (b * b - a * a) + (b - a);
 }
 
 
 double max_f1(double a, double b)
 {
 	return max(a, b) * 2.0 + 2.0;
+//	return 2;
 }
 
 double max_f2()
@@ -35,22 +39,45 @@ double max_f4()
 	return 0.0;
 }
 
-
-double I1_left(double a, double b)
+double I1_left(double a, double b, int N)
 {
-	return (b - a) * function(a);
+	double h = (b - a) / N;
+	double result = 0;
+	for (int i = 1; i <= N; ++i)
+	{
+		result = result + function(a + (i - 1) * h);
+	}
+
+	result = result * h;
+	return result;
 }
 
-double I1_trap(double a, double b)
+double I1_trap(double a, double b, int N)
 {
-	return (b - a) / 2.0 * (function(a) + function(b));
+	double h = (b - a) / N;
+	double result = function(a) + function(b);
+	for (int i = 1; i < N; ++i)
+	{
+		result = result + 2 * function(a + i * h);
+	}
+
+	result = result * (b - a) / (2 * N);
+	return result;
 }
 
-
-double I1_Simps(double a, double b)
+double I1_Simps(double a, double b, int N)
 {
-	return (b - a) / 6.0 * (function(a) + 4.0 * function((a + b) / 2.0)
-		+ function(b));
+	double h = (b - a) / (2 * N);
+	double result = function(a) + function(b);
+	for (int i = 2; i < 2 * N; i += 2)
+	{
+		result += 2 * function(a + i * h);
+		result += 4 * function(a + (i - 1) * h);
+	}
+
+	result += 4 * function(a + (2 * N - 1) * h);
+	result *= (b - a) / (6 * N);
+	return result;
 }
 
 double R_left(double a, double b, double N)
@@ -86,12 +113,11 @@ double R_N_Simps(double a, double b, double N)
 
 int main()
 {
-	double a = 0.01, b = 0.03;
+	double a = 1, b = 3;
 	cout << "method  I_2    I-I_2	R_2  |  I_4  I - I_4  R_4  |  R(2)	Iut	I - Iut" << endl;
 
-	double I_2_left = I1_left(a, (a + b) / 2.0) + I1_left((a + b) / 2.0, b);
-	double I_4_left = I1_left(a, (a + b) / 4.0) + I1_left((a + b) / 4.0, (a + b) / 2.0)
-		+ I1_left((a + b) / 2.0, (a + b) * 3.0 / 4.0) + I1_left((a + b) * 3.0 / 4.0, b);
+	double I_2_left = I1_left(a, b, 2);
+	double I_4_left = I1_left(a, b, 4);
 
 	double Iut_left = I_4_left + R_N_left(a, b, 2.0);
 
@@ -101,20 +127,16 @@ int main()
 		<< R_N_left(a, b, 2.0) << "    " << Iut_left << "   " << I(a, b) - Iut_left << endl;
 
 
-	double I_2_trap = I1_trap(a, (a + b) / 2.0) + I1_trap((a + b) / 2.0, b);
-	double I_4_trap = I1_trap(a, (a + b) / 4.0) + I1_trap((a + b) / 4.0, (a + b) / 2.0)
-		+ I1_trap((a + b) / 2.0, (a + b) * 3.0 / 4.0) + I1_trap((a + b) * 3.0 / 4.0, b);
-
+	double I_2_trap = I1_trap(a, b, 2);
+	double I_4_trap = I1_trap(a, b, 4);
 	double Iut_trap = I_4_trap + R_N_trap(a, b, 2.0);
 
 	cout << "trap  " << I_2_trap << "     " << I(a, b) - I_2_trap << "   " << R_trap(a, b, 2.0) << "|  "
 		<< I_4_trap << "   " << I(a, b) - I_4_trap << "    " << R_trap(a, b, 4.0) << "  | "
 		<< R_N_trap(a, b, 2.0) << "  " << Iut_trap << "  " << I(a,b) - Iut_trap << endl;
 
-	double I_2_Simps = I1_Simps(a, (a + b) / 2.0) + I1_Simps((a + b) / 2.0, b);
-	double I_4_Simps = I1_Simps(a, (a + b) / 4.0) + I1_Simps((a + b) / 4.0, (a + b) / 2.0)
-		+ I1_Simps((a + b) / 2.0, (a + b) * 3.0 / 4.0) + I1_Simps((a + b) * 3.0 / 4.0, b);
-
+	double I_2_Simps = I1_Simps(a, b, 2);
+	double I_4_Simps = I1_Simps(a, b, 4);
 	double Iut_Simps = I_4_Simps + R_N_Simps(a, b, 2.0);
 
 	cout << "Simps  " << I_2_Simps << "    " << I(a, b) - I_2_Simps << "    " << R_Simps(a, b, 2.0) << "  |  "
