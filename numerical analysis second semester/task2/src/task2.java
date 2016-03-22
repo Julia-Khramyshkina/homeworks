141f4a70392f0e4aa21965aaf4035503e1d4b9d6
Julia-Khramyshkina <ulkin_xram@mail.ru>
2016-03-23 00:48:14 +0300

    new version

--- numerical analysis second semester/task2/src/task2.java
+++ numerical analysis second semester/task2/src/task2.java
@@ -3,23 +3,43 @@
  */
 public class task2
 {
-    static int [] Q_Cheb = {1, 15, 7, 9, 3, 13, 5, 11};
+    static int [] Q_Cheb = {1, 15, 7, 9,  3, 13,  5, 11};
     static int p = 8;
     static double eps = 0.000001;
+    static double[][] A0 = {
+            {7.35272, 0.88255, -2.270052, 1},
+            {0.88255, 5.58351, 0.528167, 0},
+            {-2.27005, 0.528167, 4.430329, 0}};
     static double[][] A = {
-            {3.278164e-8, 1.046583, -1.378574, -0.527466},
-            {1.046583, 2.975937, 0.934251, 2.526877},
-            {-1.378574, 0.934251, 4.836173, 5.165441}};
+            {7.35272, 0.88255, -2.270052, 1},
+            {0.88255, 5.58351, 0.528167, 0},
+            {-2.27005, 0.528167, 4.430329, 0}};
     static double [][] b = {
-            {-0.527466},
-            {2.526877},
-            {5.165441}};
-    static double [] b1 = { -0.27466, 2.526877, 5.165441};
+            {1},
+            {0},
+            {0}};
+    static double [] b1 = { 1, 0, 0 };
 
     static double [][] E = {
             {1.0, 0.0, 0.0},
             {0.0, 1.0, 0.0},
             {0.0, 0.0, 1.0}};
+
+    static double [][] A_simm = {
+            {1.234, 0, 0, 2.342},
+            {0, 2.342, 0, 6.234},
+            {0, 0, 3.4532, 4.55534}};
+    static double [] b_simm = { 2.342, 6.234, 4.55534};
+
+    static double [][] A_simm1 = {
+            {1.234, 0, 0, 2.342},
+            {0, 2.342, 0, 6.234},
+            {0, 0, 3.4532, 4.55534}};
+    static double [] b_simm1 = { 2.342, 6.234, 4.55534};
+
+
+    static double [] x_current = new double[3];
+    static double [] x_prev = new double[3];
 
     static double[][] Gauss1(double[][] m, int size)
     {
@@ -47,6 +67,20 @@ public class task2
 
         }
         return m;
+    }
+
+    static double[] resolve(double[][] m, int size)
+    {
+        double[] res = new double[size];
+        for (int i = size - 1; i >= 0; i--)
+        {
+            res[i] = m[i][size];
+            for (int j = i + 1; j < size; j++)
+            {
+                res[i] -= m[i][j] * res[j];
+            }
+        }
+        return res;
     }
 
     static double smallM(double[][] m, int size)
@@ -188,10 +222,8 @@ public class task2
         return max;
     }
 
-    static double [] simpleIteration(double [][] h, double[][] g, double [] x_current,
-         double[] x_prev, int size, int kMax)
+    static double [] simpleIteration(double [][] h, double[][] g, int size, int kMax)
     {
-
         for (int i = 0; i < size; ++i)
         {
             x_current[i] = 0;
@@ -203,7 +235,7 @@ public class task2
             for (int i = 0; i < size; ++i) {
                 double sum = 0;
                 for (int j = 0; j < size; ++j) {
-                    sum = h[i][j] * x_prev[i];
+                    sum += h[i][j] * x_prev[j];
                 }
 
                 x_current[i] = sum + g[i][0];
@@ -259,29 +291,31 @@ public class task2
         double value = 0.5 * Math.sqrt(bigM / smallM) * Math.log10(2 / eps);
         int count = 1;
 
-        while (count < value)
-        {
-            for (int k = 0; k < p; ++k)
+        while (count < value) {
+            for (int i = 0; i < p; ++i)
             {
-                double t_k = Math.cos(Math.PI * Q_Cheb[k] / (2 * p));
-                double tau_k = 2 / (bigM + smallM - (bigM - smallM) * t_k);
-                double [] Ax_prev = multMatrixOnVect(matrixA, x_prev, 3);
-                double [] temp = ariphmetic(Ax_prev, b_Cheb, tau_k, 3);
-                x_current = addForVectors(x_prev, temp, 3);
-                x_prev = x_current;
+                for (int k = 0; k < p; ++k)
+                {
+                    double t_k = Math.cos(Math.PI * Q_Cheb[k] / (2 * p));
+                    double tau_k = 2 / (bigM + smallM - (bigM - smallM) * t_k);
+                    double[] Ax_prev = multMatrixOnVect(matrixA, x_prev, 3);
+                    double[] temp = ariphmetic(Ax_prev, b_Cheb, tau_k, 3);
+                    x_current = addForVectors(x_prev, temp, 3);
+                    x_prev = x_current;
+                }
+
             }
             ++count;
         }
-
         return x_current;
     }
 
     static double forLusternik(double []array, int size)
     {
-        double max = array[0];
+        double max = Math.abs(array[0]);
         for (int i = 0; i < size; ++i)
         {
-            if (array[i] > max)
+            if (Math.abs(array[i]) > max)
             {
                 max = array[i];
             }
@@ -289,8 +323,25 @@ public class task2
         return max;
     }
 
+    static void printOneColumn(double []array, int size)
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            System.out.println(array[i]);
+        }
+
+        System.out.println();
+    }
+
     public static void main(String[] args)
     {
+        System.out.println("A:");
+        print(A0, 3);
+        print(Gauss1(A0, 3), 3);
+        double []solution = resolve(A0, 3);
+        System.out.println("Exactly solution");
+        printOneColumn(solution, 3);
+
         double smallM = smallM(A, 3);
         double bigM = bigM(A, 3);
 
@@ -306,39 +357,55 @@ public class task2
         double normOfH = norm(h, 3);
 
         System.out.println("Norm of matrix H = " +  normOfH);
-        double []exactValue = new double[3];
-        double [] x_current = new double[3];
-        double [] x_prev = new double[3];
-        int kMax = 10;
-        double []xFromSimpleIteration = simpleIteration(h, g,x_current, x_prev, 3, kMax);
+        double [] exactValue = solution;
+
+        int kMax = 15;
+        double []xFromSimpleIteration = simpleIteration(h, g, 3, kMax);
+        System.out.println("Simple iteration with kMax:");
+        printOneColumn(xFromSimpleIteration, 3);
         double []difference = vector(exactValue, xFromSimpleIteration, 3);
+
         double factAcc = normOfVector(difference, 3);
+        System.out.println("The actual accuracy: " + factAcc);
         double [] newG = new double[3];
         for (int i = 0; i < 3; ++i)
         {
             newG[i] = g[i][0];
         }
 
-        double apriorySimpleIt = Math.pow(normOfH, kMax) / (1 - normOfH) * normOfVector(newG, 3);
+        double apriorySimpleIt = Math.pow(normOfH, kMax - 1) / (1 - normOfH) * normOfVector(newG, 3);
+        System.out.println("The apriory evaluation: " + apriorySimpleIt);
 
         double []vect = vector(x_current, x_prev, 3);
         double aposterSimpleIt = normOfH / (1 - normOfH) * normOfVector(vect, 3);
+        System.out.println("The aposteriory evaluation: " + aposterSimpleIt);
 
-        double [] numbersOfH = {1, 2, 3};
+        double [] numbersOfH = {0.523057, -0.430826, 0.046116};
 
         double newConst = 1 / (1 - forLusternik(numbersOfH, 3));
         double [] temp = ariphmetic(x_current, x_prev, newConst, 3);
         double [] moreExactlyRes = addForVectors(x_prev, temp, 3);
-
+        System.out.println("More exactly solution (Lusternik)");
+        printOneColumn(moreExactlyRes, 3);
 
         double [] x1 = new double[3];
         double [] x2 = new double[3];
 
-        double [] fromCheb = iterationWithCheb(smallM, bigM, x1, x2, A, b1);
+        System.out.println("A:");
+        print(A_simm, 3);
+        print(Gauss1(A_simm, 3), 3);
+        double []sol1 = resolve(A_simm, 3);
+        System.out.println("Exactly solution");
+        printOneColumn(sol1, 3);
+
+
+        double [] fromCheb = iterationWithCheb(smallM(A_simm1, 3), bigM(A_simm1, 3), x1, x2, A_simm1, b_simm1);
+        System.out.println("Solution from Cheb params");
+        printOneColumn(fromCheb, 3);
 
         double [] newDifference = vector(exactValue, fromCheb, 3);
         double factAcc2 = normOfVector(newDifference, 3);
 
-        System.out.println("The actual accuracy:");
+        System.out.println("The actual accuracy:" + factAcc2);
     }
 }
